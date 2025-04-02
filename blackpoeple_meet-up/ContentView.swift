25a5fb3597d8c56af9f656c0b971410b47256698
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2025-04-02 10:07:45 -0700

    button lable action

--- blackpoeple_meet-up/ContentView.swift
+++ blackpoeple_meet-up/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-        var body: some View {
-            Text("Ios emulator Xcode")
-                .padding()
-            Text("Test-commments")
-                .opacity(0);
-           // var bucketList: [String]
-            
-    
+    var body: some View {
+        Text("Black poeple meet up")
+            .frame(width: 200,height:70)
+            .border(Color.green)
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+        //action onClick
         }
+    }
+    
     
 }
 
